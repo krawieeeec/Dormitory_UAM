85b0db87c72c12647b45ee34927b6d0cca61462d
Dawid <dk62344@st.amu.edu.pl>
2017-06-08 12:59:46 +0200

    uninstall pg module with dependencies from project

--- app/api/model/server.js
+++ app/api/model/server.js
@@ -5,24 +5,15 @@ const pool = require('../../../db');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
-    
+
     pool.query('CREATE DATABASE mydb', function(err,res){
         if(err) {
-            return pool.query('DROP DATABASE mydb', function(){
+             pool.query('DROP DATABASE mydb', function(){
                 console.log('usunięto bazę');
             });
         };
         console.log('utworzono bazę');
-    })
-    /*
-    pool.query('SELECT $1::int AS number', ['2'], function(err, res) {
-  if(err) {
-    return console.error('error running query', err);
-  }
-
-  console.log('number:', res.rows[0].number);
-});
-*/
+    });
 })
 
 app.listen(3000);
