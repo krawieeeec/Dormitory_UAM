07d3abbcfc081336add27b6219abd38d873f56cd
Dawid <dk62344@st.amu.edu.pl>
2017-11-10 15:04:30 +0100

    create pesel validation on personal data

--- app/client/src/app/components/resident-list/resident-add/resident-personal-data/resident-personal-data.component.ts
+++ app/client/src/app/components/resident-list/resident-add/resident-personal-data/resident-personal-data.component.ts
@@ -90,9 +90,15 @@ export class ResidentPersonalDataComponent implements OnChanges, OnInit, DoCheck
         surname: true,
         birthPlace: true,
         fatherName: true,
-        motherName: true
+        motherName: true,
       },
-      phoneNumber: true
+      pesel: {
+        specialCharactersAndNonDigitCharacters: true,
+        incorrectPesel: true,
+        whiteSpaces: true
+      },
+      phoneNumber: true,
+      
     }
   }
 
@@ -171,68 +177,109 @@ export class ResidentPersonalDataComponent implements OnChanges, OnInit, DoCheck
   }
 
   CheckValidation(input, typeInput){
-    
-    var characters = [], specialCharacters = [], numbers = [], stringWithoutWhiteSpace = '';
+    console.log(this.residentPersonalData.birthDate);
+    console.log(typeof this.residentPersonalData.birthDate);
+    var phoneNumber = [], specialCharactersInInput = [], numbersInInput = [], stringWithoutWhiteSpace = '',
+    nonDigitcharactersInInput = [];
 
     stringWithoutWhiteSpace = input.replace(/\s/g,'')
 
-    specialCharacters = stringWithoutWhiteSpace.match(/\W/g);
-    numbers = stringWithoutWhiteSpace.match(/\d/g);
-    characters = input.match(/\+[0-9]*\s*[0-9]*/g); 
+    specialCharactersInInput = stringWithoutWhiteSpace.match(/\W/g);
+    numbersInInput = stringWithoutWhiteSpace.match(/\d/g);
+    nonDigitcharactersInInput = input.match(/\D/g);
+    phoneNumber = input.match(/\+[0-9]*\s*[0-9]*/g); 
     
     if(typeInput.name == 'name'){
-      if((specialCharacters != null || numbers != null)){
+      if((specialCharactersInInput != null || numbersInInput != null)){
         this.validationError.specialCharactersOrNumbers.name = false;
-      }else if((specialCharacters == null && numbers == null)){
+      }else if((specialCharactersInInput == null && numbersInInput == null)){
         this.validationError.specialCharactersOrNumbers.name = true;
       }
-    }
-    
-    if(typeInput.name == 'surname'){
-      if((specialCharacters != null || numbers != null)){
+    }else if(typeInput.name == 'surname'){
+      if((specialCharactersInInput != null || numbersInInput != null)){
         this.validationError.specialCharactersOrNumbers.surname = false;
-      }else if((specialCharacters == null && numbers == null)){
+      }else if((specialCharactersInInput == null && numbersInInput == null)){
         this.validationError.specialCharactersOrNumbers.surname = true;
       }
-    }
-
-    if(typeInput.name == 'birthPlace'){
-      if((specialCharacters != null || numbers != null)){
+    }else if(typeInput.name == 'birthPlace'){
+      if((specialCharactersInInput != null || numbersInInput != null)){
         this.validationError.specialCharactersOrNumbers.birthPlace = false;
-      }else if((specialCharacters == null && numbers == null)){
+      }else if((specialCharactersInInput == null && numbersInInput == null)){
         this.validationError.specialCharactersOrNumbers.birthPlace = true;
       }
-    }
-
-    if(typeInput.name == 'fatherName'){
-      if((specialCharacters != null || numbers != null)){
+    }else if(typeInput.name == 'fatherName'){
+      if((specialCharactersInInput != null || numbersInInput != null)){
         this.validationError.specialCharactersOrNumbers.fatherName = false;
-      }else if((specialCharacters == null && numbers == null)){
+      }else if((specialCharactersInInput == null && numbersInInput == null)){
         this.validationError.specialCharactersOrNumbers.fatherName = true;
       }
-    }
-
-    if(typeInput.name == 'motherName'){
-      if((specialCharacters != null || numbers != null)){
+    }else if(typeInput.name == 'motherName'){
+      if((specialCharactersInInput != null || numbersInInput != null)){
         this.validationError.specialCharactersOrNumbers.motherName = false;
-      }else if((specialCharacters == null && numbers == null)){
+      }else if((specialCharactersInInput == null && numbersInInput == null)){
         this.validationError.specialCharactersOrNumbers.motherName = true;
       }
-    }
-    
-    if(typeInput.name == 'phoneNumber'){
-      if(characters != null){
-        if(input == characters[0]){
+    }else if(typeInput.name == 'phoneNumber'){
+      if(phoneNumber != null){
+        if(input == phoneNumber[0]){
           this.validationError.phoneNumber = true;
         }else{
           this.validationError.phoneNumber = false;
         }
-      }else if((characters == null) && (input != '')){
+      }else if((phoneNumber == null) && (input != '')){
         this.validationError.phoneNumber = false;
-      }else if((characters == null) && (input == '')){
+      }else if((phoneNumber == null) && (input == '')){
         this.validationError.phoneNumber = true;
-      }      
-    }
+      }
+    }else if(typeInput.name == 'pesel'){
+      if(nonDigitcharactersInInput != null){
+        this.validationError.pesel.specialCharactersAndNonDigitCharacters = false;
+      }else{
+        this.validationError.pesel.specialCharactersAndNonDigitCharacters = true;
+      }
+      if((this.residentPersonalData.pesel.length < 11) && (this.validationError.pesel.incorrectPesel == false)){
+        this.validationError.pesel.incorrectPesel = true;
+      }
+      if(this.residentPersonalData.pesel.length == 11){
+        let peselExpression = 0, peselControlDigit = 0;
+
+        if((this.residentPersonalData.pesel[2] == '8') || ((this.residentPersonalData.pesel[2] == '9'))){
+          console.log('Rok 1800-1899');
+        }else if((this.residentPersonalData.pesel[2] == '0') || (this.residentPersonalData.pesel[2] == '1')){
+          console.log('Rok 1900-1999');
+        }else if((this.residentPersonalData.pesel[2] == '2') || (this.residentPersonalData.pesel[2] == '3')){
+          console.log('Rok 2000-2099');
+        }else if((this.residentPersonalData.pesel[2] == '4') || (this.residentPersonalData.pesel[2] == '5')){
+          console.log('Rok 2100-2199');
+        }else if((this.residentPersonalData.pesel[2] == '6') || (this.residentPersonalData.pesel[2] == '7')){
+          console.log('Rok 2200 - 2299');
+        }
+        if(
+          (this.residentPersonalData.pesel[9] == '1') || (this.residentPersonalData.pesel[9] == '3') ||
+          (this.residentPersonalData.pesel[9] == '5') || (this.residentPersonalData.pesel[9] == '7') ||
+          (this.residentPersonalData.pesel[9] == '9')
+        ){
+          console.log('Płeć męska');
+        }else{
+          console.log('Płeć żeńska');
+        }
+
+        peselExpression = (
+          (9 * this.residentPersonalData.pesel[0]) + (7 * this.residentPersonalData.pesel[1]) + 
+          (3 * this.residentPersonalData.pesel[2]) + (1 * this.residentPersonalData.pesel[3]) +
+          (9 * this.residentPersonalData.pesel[4]) + (7 * this.residentPersonalData.pesel[5]) + 
+          (3 * this.residentPersonalData.pesel[6]) + (1 * this.residentPersonalData.pesel[7]) +
+          (9 * this.residentPersonalData.pesel[8]) + (7 * this.residentPersonalData.pesel[9]));
+         
+          peselControlDigit = (peselExpression % 10);
+          
+          if(peselControlDigit != this.residentPersonalData.pesel[10]){
+            this.validationError.pesel.incorrectPesel = false;
+          }else{
+            this.validationError.pesel.incorrectPesel = true;
+          }
+      }
+    } 
 
   }
 
